001fc83abd5572851276b09f65e4b923f262d6bd
Michael Shachar <michael101sh@gmail.com>
2026-02-09 10:50:06 +0200

    Basic codebase built

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Code Ocean Challenge</h1>
+      <h2>
+        In this challenge you will create a React app with a virtualized
+        infinite scroll list.
+      </h2>
+    </div>
+  );
+};
+
+export default App;
